5f1ae5907914ecd05a4d15044106f217bb69c8b7
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-01 14:24:56 +0100

    Add json_object_to_string with only one element

--- lib/json/src/json_to_string/json_object_to_string.c
+++ lib/json/src/json_to_string/json_object_to_string.c
@@ -10,6 +10,38 @@
 #include "list.h"
 #include "json.h"
 
+char *jo_to_string_add_element(char *str, json_element_t *je)
+{
+    char *tmp = json_element_to_string(je);
+
+    if (!tmp) {
+        free(str);
+        return (NULL);
+    }
+    if (json_to_string_append(&str, tmp)) {
+        free(tmp);
+        return (NULL);
+    }
+    free(tmp);
+    return (str);
+}
+
+char *jo_to_string_add_elements(json_object_t *jo, char *str)
+{
+    simple_list_t *elements;
+    json_element_t *je;
+
+    elements = jo->elements->list;
+    while (elements) {
+        je = (json_element_t *)elements->data;
+        str = jo_to_string_add_element(str, je);
+        if (!str)
+            return (NULL);
+        elements = elements->next;
+    }
+    return (str);
+}
+
 char *json_object_to_string(json_object_t *jo)
 {
     char *str = NULL;
@@ -17,6 +49,9 @@ char *json_object_to_string(json_object_t *jo)
     if (!jo)
         return (NULL);
     if (json_to_string_append(&str, "{"))
+        return (NULL);
+    str = jo_to_string_add_elements(jo, str);
+    if (!str)
         return (NULL);
     if (json_to_string_append(&str, "}"))
         return (NULL);
--- tests/tests_to_string/tests_json_object_to_string.c
+++ tests/tests_to_string/tests_json_object_to_string.c
@@ -28,3 +28,122 @@ Test(json_object_to_string, empty)
     json_object_destroy(jo);
     free(str);
 }
+
+Test(json_object_to_string, one_j_null)
+{
+    char *key = "key";
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":null}";
+    char *str;
+
+    json_object_put_null(jo, key);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_bool_0)
+{
+    char *key = "key";
+    int value = 0;
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":false}";
+    char *str;
+
+    json_object_put_bool(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_bool_1)
+{
+    char *key = "key";
+    int value = 1;
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":true}";
+    char *str;
+
+    json_object_put_bool(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_int_0)
+{
+    char *key = "key";
+    int value = 144;
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":144}";
+    char *str;
+
+    json_object_put_int(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_int_1)
+{
+    char *key = "key";
+    int value = 2147483647;
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":2147483647}";
+    char *str;
+
+    json_object_put_int(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_int_2)
+{
+    char *key = "key";
+    int value = -2147483648;
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":-2147483648}";
+    char *str;
+
+    json_object_put_int(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_string)
+{
+    char *key = "key";
+    char *value = "a random string";
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":\"a random string\"}";
+    char *str;
+
+    json_object_put_string(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
+
+Test(json_object_to_string, one_j_string_escape)
+{
+    char *key = "key";
+    char *value = "\b\f\n\r\t\"\\";
+    json_object_t *jo = json_object_create();
+    char *expect = "{\"key\":\"\\b\\f\\n\\r\\t\\\"\\\\\"}";
+    char *str;
+
+    json_object_put_string(jo, key, value);
+    str = json_object_to_string(jo);
+    cr_assert_str_eq(str, expect);
+    json_object_destroy(jo);
+    free(str);
+}
