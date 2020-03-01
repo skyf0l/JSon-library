52277a8d147f46f6d14a73affe94c38be9d718b2
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-01 18:58:31 +0100

    Add json_array_to_string

--- lib/json/src/json_to_string/json_array_to_string.c
+++ lib/json/src/json_to_string/json_array_to_string.c
@@ -11,6 +11,42 @@
 #include "private_json.h"
 #include "json.h"
 
+char *ja_to_string_add_element(char *str, json_element_t *je)
+{
+    char *tmp = json_element_to_string(je);
+
+    if (!tmp) {
+        free(str);
+        return (NULL);
+    }
+    if (json_to_string_append(&str, tmp)) {
+        free(tmp);
+        return (NULL);
+    }
+    free(tmp);
+    return (str);
+}
+
+char *ja_to_string_add_elements(json_array_t *ja, char *str)
+{
+    simple_list_t *elements;
+    json_element_t *je;
+    int id = 0;
+
+    elements = ja->elements->list;
+    while (elements) {
+        je = (json_element_t *)elements->data;
+        if (id && json_to_string_append(&str, ", "))
+            return (NULL);
+        str = ja_to_string_add_element(str, je);
+        if (!str)
+            return (NULL);
+        elements = elements->next;
+        id++;
+    }
+    return (str);
+}
+
 char *json_array_to_string(json_array_t *ja)
 {
     char *str = NULL;
@@ -18,6 +54,9 @@ char *json_array_to_string(json_array_t *ja)
     if (!ja)
         return (NULL);
     if (json_to_string_append(&str, "["))
+        return (NULL);
+    str = ja_to_string_add_elements(ja, str);
+    if (!str)
         return (NULL);
     if (json_to_string_append(&str, "]"))
         return (NULL);
--- tests/tests_to_string/tests_json_array_to_string.c
+++ tests/tests_to_string/tests_json_array_to_string.c
@@ -28,3 +28,47 @@ Test(json_array_to_string, empty)
     json_array_destroy(ja);
     free(str);
 }
+
+Test(json_array_to_string, one_j_null)
+{
+    json_array_t *ja = json_array_create();
+    char *expect = "[null]";
+    char *str;
+
+    json_array_put_null(ja);
+    str = json_array_to_string(ja);
+    cr_assert_str_eq(str, expect);
+    json_array_destroy(ja);
+    free(str);
+}
+
+Test(json_array_to_string, multi_1)
+{
+    json_array_t *ja = json_array_create();
+    char *expect = "[null, 5, false]";
+    char *str;
+
+    json_array_put_null(ja);
+    json_array_put_bool_at(ja, 0, 1);
+    json_array_put_int_at(ja, 5, 1);
+    str = json_array_to_string(ja);
+    cr_assert_str_eq(str, expect);
+    json_array_destroy(ja);
+    free(str);
+}
+
+Test(json_array_to_string, multi_2)
+{
+    json_array_t *ja = json_array_create();
+    char *expect = "[null, [null, 5, {}], 5, {}]";
+    char *str;
+
+    json_array_put_null(ja);
+    json_array_put_json_object(ja, json_object_create());
+    json_array_put_int_at(ja, 5, 1);
+    json_array_put_json_array_at(ja, json_array_clone(ja), 1);
+    str = json_array_to_string(ja);
+    cr_assert_str_eq(str, expect);
+    json_array_destroy(ja);
+    free(str);
+}
