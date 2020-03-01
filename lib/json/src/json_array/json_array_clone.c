3d68aaef5284c4fd6dfb965aa935273694f865ac
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-01 16:50:32 +0100

    Update json_array_clone

--- lib/json/src/json_array/json_array_clone.c
+++ lib/json/src/json_array/json_array_clone.c
@@ -10,6 +10,25 @@
 #include "list.h"
 #include "json.h"
 
+json_array_t *ja_clone_add_data(json_array_t *ja_clone, json_array_t *ja)
+{
+    simple_list_t *elements;
+    json_element_t *je;
+    json_element_t *je_clone;
+
+    elements = ja->elements->list;
+    while (elements) {
+        je = (json_element_t *)elements->data;
+        je_clone = json_element_clone(je);
+        if (!je_clone)
+            return (json_array_destroy(ja_clone));
+        list_add(ja_clone->elements, je_clone);
+        ja_clone->elements_count++;
+        elements = elements->next;
+    }
+    return (ja_clone);
+}
+
 json_array_t *json_array_clone(json_array_t *ja)
 {
     json_array_t *ja_clone = NULL;
@@ -19,5 +38,5 @@ json_array_t *json_array_clone(json_array_t *ja)
     ja_clone = json_array_create();
     if (!ja_clone)
         return (NULL);
-    return (ja_clone);
+    return (ja_clone_add_data(ja_clone, ja));
 }
