0bdf012f8e74c05b4570282c73945d872531905b
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-01 16:55:59 +0100

    Add tests for json_array_clone

--- tests/tests_json_array/tests_json_array_create.c
+++ tests/tests_json_array/tests_json_array_create.c
@@ -17,3 +17,22 @@ Test(json_array_create, just_create)
     cr_assert_not_null(ja->elements);
     json_array_destroy(ja);
 }
+
+Test(json_array_clone, null)
+{
+    json_array_t *clone = json_array_clone(NULL);
+
+    cr_assert_null(clone);
+}
+
+Test(json_array_clone, just_create)
+{
+    json_array_t *ja = json_array_create();
+    json_array_t *clone = json_array_clone(ja);
+
+    json_array_destroy(ja);
+    cr_assert_not_null(clone);
+    cr_assert_eq(clone->elements_count, 0);
+    cr_assert_not_null(clone->elements);
+    json_array_destroy(clone);
+}
