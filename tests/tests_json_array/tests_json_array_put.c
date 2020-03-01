e828864714450fabecdc620024eed380ca3e0ee0
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-01 18:12:36 +0100

    Add json_array_put_null

--- tests/tests_json_array/tests_json_array_put.c
+++ tests/tests_json_array/tests_json_array_put.c
@@ -9,3 +9,101 @@
 #include "list.h"
 #include "../../lib/json/include/private/private_json.h"
 #include "json.h"
+
+Test(json_array_put_null, j_null)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null(ja);
+    json_element_t *je = (json_element_t *)ja->elements->list->data;
+
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 1);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    cr_assert_null(je->json_array);
+    cr_assert_null(je->json_object);
+    cr_assert_eq(je->json_int, 0);
+    cr_assert_null(je->json_string);
+    json_array_destroy(ja);
+}
+
+Test(json_array_put_null, two_j_null)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null(ja);
+    json_element_t *je = (json_element_t *)ja->elements->list->data;
+
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 1);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    rtn_value = json_array_put_null(ja);
+    je = (json_element_t *)ja->elements->list->data;
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 2);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    json_array_destroy(ja);
+}
+
+Test(json_array_put_null_at, j_null_valid_id)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null_at(ja, 0);
+    json_element_t *je = (json_element_t *)ja->elements->list->data;
+
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 1);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    cr_assert_null(je->json_array);
+    cr_assert_null(je->json_object);
+    cr_assert_eq(je->json_int, 0);
+    cr_assert_null(je->json_string);
+    json_array_destroy(ja);
+}
+
+Test(json_array_put_null_at, j_null_invalid_id_0)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null_at(ja, -1);
+
+    cr_assert_eq(rtn_value, EXIT_FAILURE);
+    cr_assert_eq(ja->elements_count, 0);
+    json_array_destroy(ja);
+}
+
+Test(json_array_put_null_at, j_null_invalid_id_1)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null_at(ja, 1);
+
+    cr_assert_eq(rtn_value, EXIT_FAILURE);
+    cr_assert_eq(ja->elements_count, 0);
+    json_array_destroy(ja);
+}
+
+Test(json_array_put_null_at, two_j_null)
+{
+    json_array_t *ja = json_array_create();
+    int rtn_value = json_array_put_null_at(ja, 0);
+    json_element_t *je = (json_element_t *)ja->elements->list->data;
+
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 1);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    rtn_value = json_array_put_null_at(ja, 0);
+    je = (json_element_t *)ja->elements->list->data;
+    cr_assert_eq(rtn_value, EXIT_SUCCESS);
+    cr_assert_eq(ja->elements_count, 2);
+    cr_assert_not_null(je);
+    cr_assert_eq(je->type, j_null);
+    cr_assert_null(je->key);
+    json_array_destroy(ja);
+}
