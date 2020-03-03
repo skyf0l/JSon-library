edd3f221c9ab9aab7d9c9981c5500e51fdad5332
Tom RORATO <tom.rorato@localhost.localdomain>
2020-03-03 19:36:37 +0100

    Fix overflow detection

--- lib/json/src/j_str/j_str_isint.c
+++ lib/json/src/j_str/j_str_isint.c
@@ -15,11 +15,15 @@ int j_str_isint(char *str)
 {
     if (!str || !*str)
         return (0);
-    if ((*str == '-' && j_strlen(str) > 11) || (*str != '-' && j_strlen(str) > 10))
+    if ((*str == '-' && j_strlen(str) > 11)
+        || (*str != '-' && j_strlen(str) > 10))
         return (0);
     for (int k = (*str == '-'); str[k]; k++)
         if (str[k] < '0' || str[k] > '9')
             return (0);
+    if ((*str == '-' && j_strlen(str) < 10)
+        || (*str != '-' && j_strlen(str) < 9))
+        return (1);
     if (*str == '-' && j_strcmp(&str[1], "2147483648") > 0)
         return (0);
     else if (*str != '-' && j_strcmp(str, "2147483647") > 0)
