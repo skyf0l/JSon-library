860c5b38a936890768e6ba394a25cb5abba97aaf
Tom RORATO <tom.rorato@desktop-5vob7t1.home>
2020-03-03 23:49:30 +0100

    Add my atoi

--- lib/json/src/j_str/j_atoi.c
+++ lib/json/src/j_str/j_atoi.c
@@ -7,5 +7,16 @@
 
 int j_atoi(char *str)
 {
-    return (atoi(str));
+    int number = 0;
+    int is_neg = str[0] == '-';
+    int i = str[0] == '-';
+
+    for (; str[i] >= '0' && str[i] <= '9'; i++) {
+        number *= 10;
+        if (is_neg)
+            number -= str[i] - 48;
+        else
+            number += str[i] - 48;
+    }
+    return (number);
 }
